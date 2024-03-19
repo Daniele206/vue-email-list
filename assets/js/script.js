192058430c7d528db2cd2ab3db60e8651fd59377
Daniele206 <danieledeieso2004@gmail.com>
2024-03-19 15:14:07 +0100

    [add] stamp 10 mail when all 10 are ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,23 @@ createApp({
     return{
       title: 'Vue Email FileList',
       listMail: [],
-      // loadMessage: ''
+      clicked: false,
+      loadMessage: 'Loading...'
     }
   },
 
   methods:{
     getMail(){
-      for(let i = 0; i < 10; i++){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(res => {
-          console.log(res.data.response);
-          this.listMail.push(res.data.response);
-        });
-        // if(listMail.length < 10){
-        //   loadMessage= 'Loading...'
-        // }
+      if(this.listMail.length < 1){
+        for(let i = 0; i < 10; i++){
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then(res => {
+            console.log(res.data.response);
+            this.listMail.push(res.data.response);
+          });
+        }
       }
+      this.clicked = true;
     }
 
   },
