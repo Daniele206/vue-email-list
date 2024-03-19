2f1d6391513f43b23d4559c5d9f45c56793155e8
Daniele206 <danieledeieso2004@gmail.com>
2024-03-19 15:19:23 +0100

    [add] 10 different emails btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ createApp({
       title: 'Vue Email FileList',
       listMail: [],
       clicked: false,
+      click: 0,
       loadMessage: 'Loading...'
     }
   },
@@ -23,8 +24,21 @@ createApp({
         }
       }
       this.clicked = true;
-    }
+      this.click++
+    },
 
+    getNewMail(){
+      this.listMail.splice(0, this.listMail.length)
+      if(this.listMail.length < 1){
+        for(let i = 0; i < 10; i++){
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then(res => {
+            console.log(res.data.response);
+            this.listMail.push(res.data.response);
+          });
+        }
+      }
+    }
   },
 
   mounted(){
