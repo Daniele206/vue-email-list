d722eec105b8ef19c0e642ace7f5087645d954cd
Daniele206 <danieledeieso2004@gmail.com>
2024-03-19 14:59:44 +0100

    [add] mail when click-btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,27 @@ const { createApp } = Vue;
 createApp({
 
   data(){
-
+    return{
+      title: 'Vue Email FileList',
+      listMail: [],
+    }
   },
 
   methods:{
     getMail(){
 
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(res => {
+        console.log(res.data.response);
+        this.listMail.push(res.data.response);
+      })
 
     }
 
   },
+
+  mounted(){
+
+  }
 
 }).mount('#app');
