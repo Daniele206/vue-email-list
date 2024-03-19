ba9e71961b69ff5325afd2aee3dbd6b56f63b4ce
Daniele206 <danieledeieso2004@gmail.com>
2024-03-19 15:03:47 +0100

    [add] 10 mail a click

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,22 @@ createApp({
     return{
       title: 'Vue Email FileList',
       listMail: [],
+      // loadMessage: ''
     }
   },
 
   methods:{
     getMail(){
-
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then(res => {
-        console.log(res.data.response);
-        this.listMail.push(res.data.response);
-      })
-
+      for(let i = 0; i < 10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(res => {
+          console.log(res.data.response);
+          this.listMail.push(res.data.response);
+        });
+        // if(listMail.length < 10){
+        //   loadMessage= 'Loading...'
+        // }
+      }
     }
 
   },
